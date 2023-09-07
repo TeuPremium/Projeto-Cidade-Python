7deb38137c0536c72d2122152091daa4727fac82
Lucianojrs <lucianojosejr1587@gmail.com>
2023-09-07 19:18:22 -0300

    primeiro commit do git

---
+++ Project.py
@@ -0,0 +1,25 @@
+class Car:
+    def __init__(self, x_pos : int, y_pos : int):
+        self.x = x_pos
+        self.y = y_pos
+        self.speed = 0
+        self.acceleration = 0
+
+    def move(x_pos : int, y_pos : int):
+        pass
+
+    def change_speed():
+        pass
+
+
+class Person:
+    def __init__(self, x_pos : int, y_pos : int):
+        self.x = x_pos
+        self.y = y_pos
+
+    def call(self):
+        pass
+
+class MQTT:
+    def __init__(self):
+        pass
