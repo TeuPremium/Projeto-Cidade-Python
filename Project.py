dcccfaf425a7f2944825ac29436858c001de8a72
Lucianojrs <lucianojosejr1587@gmail.com>
2023-09-10 23:10:24 -0300

    parte1

--- Project.py
+++ Project.py
@@ -1,26 +1,54 @@
 class Car:
-    def __init__(self, x_pos : int, y_pos : int):
-        self.x = x_pos
-        self.y = y_pos
+    def __init__(self, id, velocidade_max):
+        self.id = id    
+        self.x = 0
+        self.y = 0
         self.speed = 0
-        self.acceleration = 2
+        self.acceleration = 0
+        self.velocidade_max = velocidade_max
+        self.destiny = (0,0)
+        self.occupied = False 
 
-    def move(x_pos : int, y_pos : int):
-        pass
+    def limit_speed(self, new_speed):
+        if new_speed <= self.velocidade_max:
+            self.speed = new_speed
+        else:
+            self.speed =  self.velocidade_max
+    
+    def change_speed(self, received_speed):
+        self.speed = received_speed 
+    
+    #Duvida_implementar_logica
+    def occupy_car(self):
+        self.occupied = True  
 
-    def change_speed():
-        pass
+    def safe_distance(self, car2):
+        distnc = car2.x - self.x
+        bool = distnc >= #largura de faixa
+        if bool == False:
+            self.speed
 
+    
+
+
+
+
+    
 
 class Person:
-    def __init__(self, x_pos : int, y_pos : int):
-        self.street_width
-        self.x = x_pos
-        self.y = y_pos
 
-    def call(self):
-        pass
+    def __init__(self, id):
+        self.x = 0
+        self.y = 0
+        self.id = id
+        self.in_car = False
+        self.destiny = (0,0)
 
-class MQTT:
-    def __init__(self):
-        pass
+    def person_destiny(self, x, y):
+        self.destino = (x,y)
+
+    def inside_car(self):
+        self.in_car = True
+    
+    def outside_car(self):
+        self.in_car = False
